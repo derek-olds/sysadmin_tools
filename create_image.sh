60399ba8ac63df4d148f15c2870edc4ca0f4b022
olds <olds@testrig.doghouse>
2018-11-06 22:25:41 -0700

    Start over: Added create_image function

--- create_image.sh
+++ create_image.sh
@@ -1,54 +1,27 @@
 #!/usr/bin/bash
 set -e
-# Script to start an OS install on a "raw" formatted disk image file.
-# Arguments:
-#    $1 Command
-#    $2 Path to Image file
-#    $3 Path to installer ISO
+# Script to manage kvm images.
 
 ## Functions ##
-image_file () {
-    qemu-img create -f raw $1 100G
+create_image () {
+  virt-install\
+  --virt-type=kvm\
+  --name centos7\
+  --ram 2048\
+  --vcpus=1\
+  --os-variant=centos7.0\
+  --cdrom="$2"\
+  --network=bridge=virbr0,model=virtio\
+  --graphics vnc\
+  --disk path="$1",size=40,bus=virtio,format=raw
 }
-
-boot_installer () {
-    /usr/libexec/qemu-kvm --name installer -m 2048 -hda $1 -cdrom $2 -boot d
-}
-
-boot_image () {
-    /usr/libexec/qemu-kvm --name Test_Image -m 2048 -hda $1 -boot c
-}
-
 
 ## Main ##
 case "$1" in
-  image_file)
-    if [[ -a $2 ]]; then 
-      echo $"image file already exists."
-      exit 1
-    fi
-    image_file $2
-    ;;
-  boot_installer)
-    if [[ ! -a $2 ]]; then
-      echo $"Image file not found."
-      exit 1
-    fi
-    if [[ ! -a $3 ]]; then
-      echo $"ISO file not found."
-      exit 1
-    fi
-    boot_installer $2 $3
-    ;;
-  boot_image)
-    if [[ ! -a $2 ]]; then
-      echo $"Image file not found"
-      exit 1
-    fi
-    boot_image $2
+  create_image)
+    create_image $2 $3
     ;;
   *)
-    echo $"Usage: $0 {image_file|boot_installer|boot_image} \
-         <image file name> <path to iso>"
+    echo $"Usage: $0 {create_image} <image_file_path> <iso_file_path>"
     ;;
-esac   
+esac
