e399730e6b156ba5dc26cb75029f0a597449eaf4
olds <olds@testrig.doghouse>
2018-11-21 14:09:03 -0700

    Add support for VM name argument at $1. This will allow creating multiple VMs.

--- create_image.sh
+++ create_image.sh
@@ -6,22 +6,22 @@ set -e
 create_image () {
   virt-install\
   --virt-type=kvm\
-  --name centos7\
+  --name "$1"\
   --ram 2048\
   --vcpus=1\
   --os-variant=centos7.0\
-  --cdrom="$2"\
+  --cdrom="$3"\
   --network=bridge=virbr0,model=virtio\
   --graphics vnc\
-  --disk path="$1",size=40,bus=virtio,format=raw
+  --disk path="$2",size=40,bus=virtio,format=raw
 }
 
 ## Main ##
 case "$1" in
   create_image)
-    create_image $2 $3
+    create_image "$2" "$3" "$4"
     ;;
   *)
-    echo $"Usage: $0 {create_image} <image_file_path> <iso_file_path>"
+    echo $"Usage: $0 create_image <vm_name> <image_file_path> [iso_file_path]"
     ;;
 esac
